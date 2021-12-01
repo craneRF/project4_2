f579efd3b97ffca23963d9e3340c26db09dd2ae1
RyosukeTago <mhf.louness1229@gmail.com>
2021-12-01 12:44:30 +0900

    弾挙動修正（途中）+エネルギー弾追加

--- src/BulletObject.cpp
+++ src/BulletObject.cpp
@@ -27,6 +27,7 @@ void BulletObject::RotateToTarget(BulletComponent * _bulletComponent, MoveCompon
 	// âÒì]
 	// êiçsï˚å¸ÇÃäpìx
 	float angle = RAD_TO_DEG * atan2f(-direction.y, direction.x);
+	angle = fmodf(angle + 360, 360);
 	if (_isImmediately)
 	{
 		_bulletComponent->gActor()->RotAngle() = angle;
@@ -34,13 +35,14 @@ void BulletObject::RotateToTarget(BulletComponent * _bulletComponent, MoveCompon
 	else
 	{
 		float deltaAngle = angle - _bulletComponent->gActor()->RotAngle();
-		if (fabsf(deltaAngle) < m_bParam.rotVal / 60)
+		if (deltaAngle < m_bParam.rotVal / 60)
 		{
 			_bulletComponent->gActor()->RotAngle() = angle;
 		}
 		else
 		{
-			if (deltaAngle > 0)
+			// óvèCê≥
+			if (deltaAngle <= 0)
 			{
 				_moveComponent->AddMoveAngle(-m_bParam.rotVal);
 			}
@@ -61,10 +63,11 @@ void NomalBullet::initialize()
 {
 	m_bParam.bulletName = "NormalBullet";
 	m_bParam.scale = { 0.5,0.5 };
-	m_bParam.speed = 200.f;
+	m_bParam.speed = 600.f;
+	//m_bParam.speed = 200.f;
 	m_bParam.rotVal = 100.f;
 	m_bParam.damage = 5;
-	m_bParam.imageName = "Arrow.png";
+	m_bParam.imageName = "6b71416463520028.png";
 }
 
 void NomalBullet::Move(BulletComponent * _bulletComponent, MoveComponent * _moveComponent)
